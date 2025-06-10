2bf1dbb43c5872f30c11d9eaf3e6864b1b0397cb
Francis <franciskarani15@gmail.com>
2025-06-10 13:49:11 +0300

    learning about first class functions

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+// write your code here
+const returnFirstTwoDrivers = function(driversArray) {
+  return driversArray.slice(0, 2);
+};
+
+
+const returnLastTwoDrivers = function(driversArray) {
+  return driversArray.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(multiplier) {
+
+  return function(fare) {
+    return fare * multiplier;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectionFunction) {
+  return selectionFunction(drivers);
+}
+
+module.exports = {
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers,
+  selectingDrivers,
+  createFareMultiplier,
+  fareDoubler,
+  fareTripler,
+  selectDifferentDrivers
+};
